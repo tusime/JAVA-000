f94384e2d30486facfe88fe4ebd7d8ff51baf755
tusi <674116920@qq.com>
2020-11-25 22:57:37 +0800

    add homework06

--- Week_06/schema.sql
+++ Week_06/schema.sql
@@ -1,6 +1,6 @@
-### 电商交易，用户、商品、订单表结构
-
-```sql
+-- ----------------------------
+-- Table structure for t_user
+-- ----------------------------
 DROP TABLE IF EXISTS `t_user`;
 CREATE TABLE `t_user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -12,9 +12,10 @@ CREATE TABLE `t_user` (
   `update_time` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 comment '用户表';
-```
 
-```sql
+-- ----------------------------
+-- Table structure for t_product
+-- ----------------------------
 DROP TABLE IF EXISTS `t_product`;
 CREATE TABLE `t_product` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -25,9 +26,10 @@ CREATE TABLE `t_product` (
   `update_time` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 comment '产品表';
-```
 
-```sql
+-- ----------------------------
+-- Table structure for t_order
+-- ----------------------------
 DROP TABLE IF EXISTS `t_order`;
 CREATE TABLE `t_order` (
   `id` bigint(20) NOT NULL,
@@ -39,15 +41,12 @@ CREATE TABLE `t_order` (
   `update_time` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 comment '订单表';
-```
 
 -- ----------------------------
-Table structure for t_order_product
-
-```sql
+-- Table structure for t_order_product
+-- ----------------------------
 DROP TABLE IF EXISTS `t_order_product`;
 CREATE TABLE `t_order_product` (
   `order_id` bigint(20) NOT NULL,
   `product_id` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 comment '订单-产品关联表';
-```
